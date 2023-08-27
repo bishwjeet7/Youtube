38eed51d207848bafda01af935e818bc8839cd4e
Bishwjeet kumar <bishwjeet7250@gmail.com>
2023-08-27 22:22:54 +0530

    Update SearhBar.jsx

--- src/components/SearchBar/SearhBar.jsx
+++ src/components/SearchBar/SearhBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SearhBar.css'
 import {FaSearch} from "react-icons/fa";
 import {BsMicFill} from "react-icons/bs";
@@ -6,16 +6,23 @@ import SearchList from './SearchList';
 
 
 function SearhBar() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchListA, setSearchList] = useState(false);
+  const TitleArray=["video1","Video 2", "Animation video", "Movies"];
   return (
     <>
         <div className='SearchBar_Container'>
             <div className="SearchBar_Container2">
                 <div className="search_div">
-                    <input type='text' className='iBox_SearchBar' placeholder='Search'></input>
-                    <FaSearch className="searchIcon_SearchBar"/>
+                    <input type='text' className='iBox_SearchBar' placeholder='Search'
+                    onChange={e=>setSearchQuery(e.target.value)} onClick={e=>setSearchList(true)}
+                    />
+                    <FaSearch className="searchIcon_SearchBar"
+                    onClick={e=>setSearchList(false)}/>
                     <BsMicFill className="Mic_SearchBar" />
                     {
-                      <SearchList/>
+                      searchQuery && searchListA &&
+                      <SearchList TitleArray={TitleArray}/>
                     }
                 </div>
             </div>
