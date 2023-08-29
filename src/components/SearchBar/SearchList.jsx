595173af9a529f2a3e1b88f722c32c3f01e6a986
Bishwjeet kumar <bishwjeet7250@gmail.com>
2023-08-29 16:57:11 +0530

    click on search list

--- src/components/SearchBar/SearchList.jsx
+++ src/components/SearchBar/SearchList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SearchList.css';
 import {FaSearch} from 'react-icons/fa'
-function SearchList({TitleArray}) {
+function SearchList({TitleArray, setSearchQuery}) {
   return (
     <>
         <div className="Container_SearchList">
           {
             TitleArray.map(m=>{
-              return <p key={m} className='titleItem'>
+              return <p key={m} onClick={e=>setSearchQuery(m)} className='titleItem'>
                 <FaSearch/> {m}
               </p>
             })
--- src/components/SearchBar/SearhBar.jsx
+++ src/components/SearchBar/SearhBar.jsx
@@ -8,21 +8,21 @@ import SearchList from './SearchList';
 function SearhBar() {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchListA, setSearchList] = useState(false);
-  const TitleArray=["video1","Video 2", "Animation video", "Movies"].filter(q=>q.includes(searchQuery));
+  const TitleArray=["video1","Video 2", "Animation video", "Movies"].filter(q=>q.toUpperCase().includes(searchQuery.toUpperCase()));
   return (
     <>
         <div className='SearchBar_Container'>
             <div className="SearchBar_Container2">
                 <div className="search_div">
                     <input type='text' className='iBox_SearchBar' placeholder='Search'
-                    onChange={e=>setSearchQuery(e.target.value)} onClick={e=>setSearchList(true)}
+                    onChange={e=>setSearchQuery(e.target.value)} value={searchQuery} onClick={e=>setSearchList(true)}
                     />
                     <FaSearch className="searchIcon_SearchBar"
                     onClick={e=>setSearchList(false)}/>
                     <BsMicFill className="Mic_SearchBar" />
                     {
                       searchQuery && searchListA &&
-                      <SearchList TitleArray={TitleArray}/>
+                      <SearchList setSearchQuery={setSearchQuery} TitleArray={TitleArray}/>
                     }
                 </div>
             </div>
