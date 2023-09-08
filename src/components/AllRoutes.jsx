0e710ac3ea24db97275fdfd0c5b3b3d88598d197
Bishwjeet kumar <bishwjeet7250@gmail.com>
2023-09-09 01:53:58 +0530

    Update AllRoutes.jsx

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from '../Pages/Home/Home';
 import {Routes, Route } from 'react-router-dom';
+import Library from '../Pages/Library/Library';
 
 
 function AllRoutes() {
   return (
     <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/library' element={<Library/>}/>
     </Routes>
   )
 }
